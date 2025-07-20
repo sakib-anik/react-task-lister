5b905d77938d6590321dd188d734456faed45e94
sakib-anik <sakib.within@gmail.com>
2025-07-21 04:17:09 +0600

    Delete ALL Fixed

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -77,6 +77,7 @@ export default function TaskBoard() {
         <SearchTask onSearch={handleSearch} />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions
+            onDeleteAll={() => setTasks([])}
             onShowModal={() => {
               setShowModal(true);
               setTaskToUpdate(null);
